98f9d19874d57ba36f2e3a296301994af7a2a6cd
junreycloudstrife <junrey.cloudstrife@gmail.com>
2020-05-12 15:48:18 +0800

    Implementing multi-line inline template and an inline style

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-events-list',
-    template: '<h1>This is EventsListComponent</h1>'
+    template: `
+    <h1>This is EventsListComponent</h1>
+    <p class="body-content">This is the EventListComponent that implements an inline template</p>
+    <p class="body-content">An inline styling also implemented now</p>
+    `,
+    styles: ['.body-content{ font-size: 25px;}']
 })
 
 export class EventsListComponent{
