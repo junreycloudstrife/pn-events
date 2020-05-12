97c0741f7d981531c1ab6dba0815a0948ecd59b8
junreycloudstrife <junrey.cloudstrife@gmail.com>
2020-05-12 15:42:33 +0800

    Created EventsListComponent and added to the view

---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-events-list',
+    template: '<h1>This is EventsListComponent</h1>'
+})
+
+export class EventsListComponent{
+
+}
