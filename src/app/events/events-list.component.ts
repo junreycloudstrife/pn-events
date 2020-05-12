ee2c3766e3d2635ba12b3b4d3266e266e01c3cc3
junreycloudstrife <junrey.cloudstrife@gmail.com>
2020-05-12 15:52:57 +0800

    Implemented external template

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-events-list',
-    template: `
-    <h1>This is EventsListComponent</h1>
-    <p class="body-content">This is the EventListComponent that implements an inline template</p>
-    <p class="body-content">An inline styling also implemented now</p>
-    `,
+    templateUrl: './events-list.component.html',
     styles: ['.body-content{ font-size: 25px;}']
 })
 
