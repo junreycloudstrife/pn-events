5b54b34dde99d4e71ec093ca3b065c9dde7b78f4
junreycloudstrife <junrey.cloudstrife@gmail.com>
2020-05-12 19:35:31 +0800

    Created AddEventComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { ParticipantsListComponent } from './participants/participants-list/participants-list.component';
+import { AddEventComponent } from './events/add-event/add-event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventsListComponent,
     EventDetailsComponent,
-    ParticipantsListComponent
+    ParticipantsListComponent,
+    AddEventComponent
   ],
   imports: [
     BrowserModule,
