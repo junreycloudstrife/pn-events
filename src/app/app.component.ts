86fef176ead97ee52da83b148cfeb02dabd85c62
junreycloudstrife <junrey.cloudstrife@gmail.com>
2020-05-12 15:17:04 +0800

    Implemented view for single event using interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pn-events';
+  event = EVENT;
 }
+
+export const EVENT = {
+  id: 0,
+  name: 'Be Angularized',
+  category: 'Talks@PN',
+  speaker: 'Juan dela Cruz',
+  emcee: 'Nanit Katipunan',
+  time: '10:00 AM',
+  date: '05/02/2020',
+  duration: 3,
+  venue: {
+    address: 'Carmelite Compound',
+    building: 'Basketball court',
+    room: ''
+  },
+  onlineUrl: '',
+  participants: [
+      {
+          fullName: 'Jun Rey Ansing',
+          position: 'Innovative Technology Trainer',
+          company: 'Passerelles numeriques'
+      },
+      {
+        fullName: 'Rene Abdallah',
+        position: 'IT/Training Manager',
+        company: 'Passerelles numeriques'
+    },
+    {
+        fullName: 'Marine Le Mezo',
+        position: 'IT Trainer/Volunteer',
+        company: 'Passerelles numeriques'
+    },
+],
+  description: 'This is all about opportunities with Angular '
+};
