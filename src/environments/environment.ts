45e5414e0bcbc28898b12572548ff9dcd3f1f0a4
junreycloudstrife <junrey.cloudstrife@gmail.com>
2020-05-13 13:32:25 +0800

    Added the Firebase module and configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyClCwXfrl_IbEEIwWVGDCV3HhSDgnYXNyk',
+    authDomain: 'pn-events.firebaseapp.com',
+    databaseURL: 'https://pn-events.firebaseio.com',
+    projectId: 'pn-events',
+    storageBucket: 'pn-events.appspot.com',
+    messagingSenderId: '900628726400',
+    appId: '1:900628726400:web:d42f3f5b72634306b65c65',
+    measurementId: 'G-8Q777XQPSJ'
+  }
 };
 
 /*
