db213965e8c04df65897d024863f9ba81a3ed723
junreycloudstrife <junrey.cloudstrife@gmail.com>
2020-05-18 22:05:23 +0800

    Added Firebase configuration for production mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyClCwXfrl_IbEEIwWVGDCV3HhSDgnYXNyk',
+    authDomain: 'pn-events.firebaseapp.com',
+    databaseURL: 'https://pn-events.firebaseio.com',
+    projectId: 'pn-events',
+    storageBucket: 'pn-events.appspot.com',
+    messagingSenderId: '900628726400',
+    appId: '1:900628726400:web:d42f3f5b72634306b65c65',
+    measurementId: 'G-8Q777XQPSJ'
+  }
 };
