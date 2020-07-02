baeb17ccdcbaf896787ef804ddfd41e396d491cc
junreycloudstrife <junrey.cloudstrife@gmail.com>
2020-07-02 11:42:30 +0800

    Implemented login form.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserAccount } from '../data-models';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  userName;
+  password;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login(value: any) {
+    console.table(value);
   }
 
 }
